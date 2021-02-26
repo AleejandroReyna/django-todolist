0847ed039e09bc141443c4a21c5bf1ca37f741a2
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-02-25 21:25:04 -0600

    Added Signup and login view, signup template and widget tweaks for forms.

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,7 @@ from pages import views
 app_name = 'pages'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    path('login/', views.LogInView.as_view(), name='login'),
+    path('signup/', views.SignUpView.as_view(), name='signup')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,23 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView as AuthLoginView, LogoutView as AuthLogoutView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 class HomeView(TemplateView):
     template_name = 'pages/home.html'
+
+
+class SignUpView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'pages/signup.html'
+
+
+class LogInView(AuthLoginView):
+    template_name = 'pages/login.html'
+
+    def get_success_url(self):
+        messages.success(self.request, "Has iniciado sesión.")
+        return super(LogInView, self).get_success_url()
