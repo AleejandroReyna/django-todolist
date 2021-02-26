16b4d1b4484aca19acb70a8f75da65656154175e
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-02-25 22:52:37 -0600

    Added status field

--- tasks/models.py
+++ tasks/models.py
@@ -3,11 +3,15 @@ from django.db import models
 import uuid
 
 
+STATUS_CHOICES = (('todo', 'To Do'), ('doing', 'Doing'), ('inreview', 'In Review'), ('finished', 'Finished'))
+
+
 class Task(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='todo')
 
     def __str__(self):
         return self.name
