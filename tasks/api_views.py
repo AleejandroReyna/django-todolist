a09e4ff067e63c192b66f1bd2730f17eba38018b
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-02-27 22:02:07 -0600

    Added decorator to check if user can delete or update the task of the view.

--- tasks/api_views.py
+++ tasks/api_views.py
@@ -1,12 +1,18 @@
+from django.utils.decorators import method_decorator
+from tasks import serializers, models, decorators
 from rest_framework import viewsets, permissions
-from tasks import serializers, models
 
 
+@method_decorator(decorators.check_owner, name='update')
+@method_decorator(decorators.check_owner, name='destroy')
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.TaskSerializer
-    queryset = models.Task.objects.all()
     permission_classes = [permissions.IsAuthenticated]
+    queryset = models.Task.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        return self.request.user.task_set.all()
 
--- tasks/decorators.py
+++ tasks/decorators.py
@@ -4,9 +4,15 @@ from django.core.exceptions import PermissionDenied
 def check_owner(function):
 
     def wrapper(request, *args, **kwargs):
+        print(kwargs)
         if not request.user.is_authenticated:
             raise PermissionDenied
-        check_task = request.user.task_set.filter(id=kwargs['task_id'])
+        if 'task_id' in kwargs:
+            check_task = request.user.task_set.filter(id=kwargs['task_id'])
+        elif 'pk' in kwargs:
+            check_task = request.user.task_set.filter(id=kwargs['pk'])
+        else:
+            raise PermissionDenied
         if not check_task.exists():
             raise PermissionDenied
         return function(request, *args, **kwargs)
