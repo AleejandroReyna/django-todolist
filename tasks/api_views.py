36c6f6d6981fd0c326b8b3510b2fbf3e3a6a8ba2
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-02-27 21:32:22 -0600

    Added user to create task.

--- tasks/api_views.py
+++ tasks/api_views.py
@@ -1,8 +1,12 @@
+from rest_framework import viewsets, permissions
 from tasks import serializers, models
-from rest_framework import viewsets
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.TaskSerializer
     queryset = models.Task.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
